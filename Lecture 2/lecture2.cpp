1d1c7264ace9f460e0f18a042eea309e75a05818
Nashid Rakib <nashidrakib@gmail.com>
2024-06-09 18:46:39 +0600

    lecture 2 update

---
+++ Lecture 2/lecture2.cpp
@@ -0,0 +1,65 @@
+// #1 recursive factorial
+
+// #include <iostream>
+// using namespace std;
+// int fact(int n){
+//     if(n == 1) return 1;
+//     return n*fact(n-1);
+// }
+// int main(){
+//     cout<<"enter the factorial value "<<endl;
+//     int n;
+//     cin>>n;
+//     cout<<"factorial is "<<fact(n)<<endl;
+//     return 0;
+// }
+
+
+
+// #2 recursive power calculation
+
+// #include <iostream>
+// using namespace std;
+// int power(int x, int y){
+//     if(y == 1) return x;
+//     return x*power(x,y-1);
+// }
+// int main(){
+//     cout<<"enter x value then y value"<<endl;
+//     int x,y;
+//     cin>>x>>y;
+//     cout<< x <<" powered "<< y << " is " <<power(x,y)<<endl;
+//     return 0;
+// }
+
+
+
+// #3 recursive fibonacci series
+
+// #include<iostream>
+// using namespace std;
+// int fibo(int n){
+//     if(n == 1) return 1;
+//     else if(n == 0) return 0; 
+//     return fibo(n-1)+fibo(n-2);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<fibo(n)<<endl;
+// }
+
+
+
+// #4 recursive palindrome
+
+// #include<iostream>
+// using namespace std;
+// char palin(char s,char r,int n){
+
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     int m = reverseNum()
+// }
